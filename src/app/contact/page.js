ae48b20754964280387122b2de2d8435fb477f4a
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2024-10-24 00:32:29 -0400

    Contact
    I am nearly done with the contact page, but I don't want to go any further without reviewing my design choices and seeing if I need to make any major changes.

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -4,8 +4,13 @@ import contactStyles from "./contact.module.css";
 
 export default function Contact() {
   return (
-    <main>
+    <main className={contactStyles.contactMain}>
       <h1>Contact</h1>
+      <input type="name" placeholder="Name"></input>
+      <input type="email" placeholder="Email"></input>
+      <textarea placeholder="Message"></textarea>
+      <button>Submit</button>
+      <div className={contactStyles.socials}></div>
     </main>
   );
 }
